faa38e49289a75c84b10b743cde45934ac1f10f8
Daniel <daniel@gmail.com>
2023-07-11 15:20:33 +0300

    re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,26 @@ from datetime import datetime
 class BaseModel:
     """Defines the BaseModel of the HBnB project."""
 
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
