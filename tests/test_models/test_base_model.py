aa365a93aba1657a554180c17b236e327ed43e1b
Emmaobi7 <emma46574@gmail.com>
2023-07-12 09:33:50 -0700

    added tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,5 +16,21 @@ class TestBaseModel(unittest.TestCase):
         """ checks for module documentation """
         self.assertTrue(len(base_model.__doc__) > 0)
 
+    def test_class_doc(self):
+        """ check for class documentation """
+        self.assertTrue(len(base_model.BaseModel.__doc__) > 0)
+
+    def test_method_doc(self):
+        """ tests for method documentation """
+        for method in dir(base_model.BaseModel):
+            self.assertTrue(len(method.__doc__) > 0)
+
+    def test_uuid(self):
+        """ tests for correct and unique uuid """
+        first = base_model.BaseModel()
+        second = base_model.BaseModel()
+        
+        self.assertNotEqual(first.id, second.id)
+
 if __name__ == '__main__':
     unittest.main()
