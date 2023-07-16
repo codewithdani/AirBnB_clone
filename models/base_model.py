a6eeb91e474f0adb429e7e3271a9fd30d6aac4b4
Emmaobi7 <emma46574@gmail.com>
2023-07-16 12:19:10 +0000

    fixing

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,42 @@
-#!/usr/bin/python3
-"""The BaseModel class."""
+#!/usr/bin/env python3
+""" module for basemodel creation """
+import datetime
+import uuid
 import models
-from uuid import uuid4
-from datetime import datetime
-
 
 class BaseModel():
-    """Defines the BaseModel of the HBnB project."""
-
+    """ The base modeks class """
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
+        """ initializes base model
         Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+            args: positional arrguments
+            kwargs: keyword aegunents
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
+
+        if kwargs:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.datetime.strptime(value, date_format)
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        models.storage.new(self)
+
+    def __str__(self):
+        """ returns in format """
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        """ updates updated_at and makes petsist"""
+        self.updated_at = datetime.datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        with the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = dict(self.__dict__)
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
-
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """ return dictionary repr of model """
+        dict_copy = dict(self.__dict__)
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
